d6460cef3a1758385623130e57365fdb0497cf45
Billieproper-bit <hedkoendi@gmail.com>
2026-02-16 22:38:46 +0100

    Delete supabaseClient.ts

--- supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://srsnjifezttivawxnndu.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyc25qaWZlenR0aXZhd3hubmR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNzY1MjcsImV4cCI6MjA3OTY1MjUyN30.5RmDp3QgAYX7yDS39KwoRtHc6c9Kp-wGKGYeJctoY7w';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
