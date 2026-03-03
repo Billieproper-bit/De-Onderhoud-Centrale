67825162d54113ea9ccfc2e1d79e8b8802d3e1ff
Billieproper-bit <hedkoendi@gmail.com>
2026-03-03 20:11:15 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,6 @@ import { supabase } from './supabase-config.js';
 // Als je de upload-functies ook apart hebt gezet:
 // import { uploadImages } from './storage-service.js';
 
-    const SUPABASE_URL = 'https://srsnjifezttivawxnndu.supabase.co';
-    const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyc25qaWZlenR0aXZhd3hubmR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNzY1MjcsImV4cCI6MjA3OTY1MjUyN30.5RmDp3QgAYX7yDS39KwoRtHc6c9Kp-wGKGYeJctoY7w';
-    var supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
     let currentUser = null;
     let currentUserRole = 'user';
     let systems = [];
